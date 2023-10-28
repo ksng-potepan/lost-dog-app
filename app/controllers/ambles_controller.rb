4b83ea585383e0d6f01fba3c98fe859d6c828145
ksng-potepan <min.fam462@gmail.com>
2023-10-28 23:39:34 +0900

    feat: ambleコントローラを追加

---
+++ app/controllers/ambles_controller.rb
@@ -0,0 +1,67 @@
+class AmblesController < ApplicationController
+  before_action :authenticate_user!
+
+  def information
+    @user = current_user
+  end
+
+  def index
+    @user = current_user
+    @ambles = Amble.all
+  end
+
+  def show
+    @user = current_user
+    @amble = Amble.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @amble = Amble.new
+  end
+
+  def edit
+    @user = current_user
+    @amble = Amble.find(params[:id])
+  end
+
+  def create
+    @user = current_user
+    @amble = Amble.new(amble_params)
+    @amble.user_id = current_user.id
+    if @amble.save
+      flash[:notice] = t('flash.notices.create')
+      redirect_to amble_path(@amble)
+    else
+      flash.now[:alert] = t('flash.alerts.create_fail')
+      render :new
+    end
+  end
+
+  def update
+    @user = current_user
+    @amble = Amble.new(room_params)
+    @amble.user_id = current_user.id
+    if @amble.update(amble_params)
+      flash[:notice] = t('flash.notices.update')
+      redirect_to amble_path(@amble)
+    else
+      flash.now[:alert] = t('flash.alerts.update_fail')
+      render :edit
+    end
+  end
+
+  def destroy
+    @amble = Amble.find(params[:id])
+    @amble.destroy
+    flash[:notice] = t('flash.notices.destroy')
+    redirect_to ambles_url(@amble)
+  end
+
+  private
+
+  def amble_params
+    params.require(:amble).permit(:name, :breed, :size, :gender, :color, :age, :features, :tag, :chip, :date, :time,
+                                  :prefecture, :municipalities, :area, :situation, :notification, :image)
+  end
+end
