abf0785a14b9619c733f16668e309d4402091b00
ksng-potepan <min.fam462@gmail.com>
2023-11-12 01:43:07 +0900

    feat: ambleのSystem Spec を追加

--- spec/factories/ambles.rb
+++ spec/factories/ambles.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :amble do
+  factory :amble,class: Amble do
     name           { "Amble" }
     breed          { "柴犬" }
     size           { "小型" }
@@ -16,5 +16,24 @@ FactoryBot.define do
     area           { "渋谷駅前" }
     situation      { "散歩中に" }
     notification   { "保健所" }
+  end
+
+  factory :other_amble,class: Amble do
+    name           { "amble" }
+    breed          { "豆柴" }
+    size           { "小型" }
+    gender         { "不明" }
+    color          { "黒色" }
+    age            { "2歳" }
+    features       { "甘えん坊" }
+    tag            { true }
+    chip           { true }
+    date           { "2000/11/11" }
+    time           { "11:11" }
+    prefecture     { "東京都" }
+    municipalities { "渋谷区" }
+    area           { "ハチ公前" }
+    situation      { "散歩中に" }
+    notification   { "警察署" }
   end
 end
