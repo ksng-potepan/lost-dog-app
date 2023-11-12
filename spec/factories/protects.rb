b27a1b4226972a71aa85d2df782b3b29fe14c729
ksng-potepan <min.fam462@gmail.com>
2023-11-12 22:59:05 +0900

    feat: protectのSystem Spec を追加

--- spec/factories/protects.rb
+++ spec/factories/protects.rb
@@ -20,28 +20,7 @@ FactoryBot.define do
     location       { nil }
   end
 
-  factory :other_protect,class: Protect do
-    name           { "protect" }
-    breed          { "プードル" }
-    size           { "小型" }
-    gender         { "不明" }
-    color          { "黒色" }
-    age            { "7歳" }
-    features       { "甘えん坊" }
-    tag            { true }
-    chip           { true }
-    date           { "2000/11/11" }
-    time           { "11:11" }
-    prefecture     { "沖縄県" }
-    municipalities { "那覇市" }
-    area           { "那覇公園" }
-    situation      { "散歩中に" }
-    notification   { "警察署" }
-    transferred    { false }
-    location       { nil }
-  end
-
-  factory :trandferred_protect,class: Protect do
+  factory :transferred_protect,class: Protect do
     name           { "ferred" }
     breed          { "ポメラニアン" }
     size           { "小型" }
