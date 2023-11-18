dc6bfc44146780298e0aef5d25d63368b8d27c5e
ksng-potepan <min.fam462@gmail.com>
2023-11-18 16:08:04 +0900

    feat: Entryモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_18_070359) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_18_070703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_18_070359) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "time"
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "protects", force: :cascade do |t|
