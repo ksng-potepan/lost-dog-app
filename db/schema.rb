47c9509294f39f830aa47dad0b3c3b34889168cb
ksng-potepan <min.fam462@gmail.com>
2023-11-18 16:06:19 +0900

    feat: Roomモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_12_071505) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_18_070359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,6 +89,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_12_071505) do
     t.text "notification"
     t.boolean "transferred", default: false
     t.text "location"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
