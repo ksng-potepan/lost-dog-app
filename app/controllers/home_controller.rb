d840907f1aa964f76c97c1a000ce1a88e6e364f1
ksng-potepan <min.fam462@gmail.com>
2024-01-18 16:23:15 +0900

    feat: トップページに目撃情報を最新３件を表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
-before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_user!, except: [:top]
 
   def top
     @user = current_user
+    @sightings = Sighting.order(created_at: :desc).limit(3)
   end
 
   def information
