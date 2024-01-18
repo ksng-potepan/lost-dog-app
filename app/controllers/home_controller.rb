c8cb0694303ef866f68431eb55d8ad26c0903a91
ksng-potepan <min.fam462@gmail.com>
2024-01-18 12:00:00 +0900

    feat:  [新規投稿]へのリンクを[top#information]に追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+before_action :authenticate_user!, except: [:top]
+
   def top
+    @user = current_user
+  end
+
+  def information
     @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post 'users/update' => 'users#update'
+  get  "information"  => "home#information"
 
   root to: 'home#top'
   devise_for :users, controllers: {
