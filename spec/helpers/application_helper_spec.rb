c006f58dfc58d67d7db48c980a8d66c9d15ff919
ksng-potepan <min.fam462@gmail.com>
2024-02-07 17:15:26 +0900

    style: ページごとのtitleを追加

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper do
   describe '#full_title' do
     it '引数がnilの時に動的な表示がされること' do
-      expect(full_title(nil)).to eq('wanwan LIFE')
+      expect(full_title(nil)).to eq('ドッグル')
     end
 
     it '引数が渡されている時に動的な表示がされること' do
-      expect(full_title('sample')).to eq('sample - wanwan LIFE')
+      expect(full_title('sample')).to eq('sample / ドッグル')
     end
 
     it '引数が空文字の時に動的な表示がされること' do
-      expect(full_title('')).to eq('wanwan LIFE')
+      expect(full_title('')).to eq('ドッグル')
     end
   end
 end
