218ebddc811f7115504ca60fd80cd64572c08525
ksng-potepan <min.fam462@gmail.com>
2024-02-29 17:55:10 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+    post '/users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   resources :users, only: [:show, :edit, :update]
   resources :messages, only: [:create]
