4bbee0ed83a6b8831ab3d8f03db0eb0241fef79a
ksng-potepan <min.fam462@gmail.com>
2024-02-29 22:35:37 +0900

    feat: テストデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,24 @@ User.create!(
   username: 'テスト太郎',
   email: 'taro@example.com',
   password: '123456',
-  password_confirmation: '123456'
+  password_confirmation: '123456',
+  id: 1
 )
 
 User.create!(
   username: 'テスト花子',
   email: 'hanako@example.com',
   password: '111111',
-  password_confirmation: '111111'
+  password_confirmation: '111111',
+  id: 2
+)
+
+User.create!(
+  username: 'ゲスト',
+  email: 'guest@example.com',
+  password: '111111',
+  password_confirmation: '111111',
+  id: 3
 )
 
 # テスト用投稿データ作成
@@ -39,7 +49,7 @@ Amble.create!(
   area: "渋谷駅前",
   situation:"散歩中に",
   notification: "保健所",
-  user_id: 4902
+  user_id: 1
 )
 
 Amble.create!(
@@ -53,13 +63,33 @@ Amble.create!(
   tag: true,
   chip: true,
   date: "2000/11/11",
-  time: "11:11",
+  time: "夕方",
   prefecture: "東京都",
   municipalities: "渋谷区",
   area: "ハチ公前",
   situation:"散歩中に",
   notification: "警察署",
-  user_id: 4903
+  user_id: 2
+)
+
+Amble.create!(
+  name: "まいご",
+  breed: "ダックスフンド",
+  size: "小型",
+  gender: "不明",
+  color: "茶色",
+  age: "6歳",
+  features: "破天荒",
+  tag: false,
+  chip: false,
+  date: "2011/10/10",
+  time: "11:11",
+  prefecture: "東京都",
+  municipalities: "渋谷区",
+  area: "スクランブル交差点",
+  situation:"移動中に",
+  notification: "保健所",
+  user_id: 3
 )
 
 Protect.create!(
@@ -81,7 +111,7 @@ Protect.create!(
   notification: "保健所",
   transferred: false,
   location: nil,
-  user_id: 4902
+  user_id: 1
 )
 
 Protect.create!(
@@ -103,11 +133,33 @@ Protect.create!(
   notification: "警察署",
   transferred: true,
   location: "近所の〇〇さん宅",
-  user_id: 4903
+  user_id: 2
+)
+
+Protect.create!(
+  name: "ほご",
+  breed: "マルチーズ",
+  size: "小型",
+  gender: "不明",
+  color: "白色",
+  age: "不明",
+  features: "おとなしい",
+  tag: false,
+  chip: false,
+  date: "2011/10/10",
+  time: "11:11",
+  prefecture: "東京都",
+  municipalities: "渋谷区",
+  area: "スクランブル交差点",
+  situation: "移動中に",
+  notification: "保健所",
+  transferred: true,
+  location: "自治体の保健所",
+  user_id: 3
 )
 
 Sighting.create!(
-  user_id: 4903,
+  user_id: 1,
   date: "2000/11/11",
   time: "正午",
   area: "ハチ公前",
@@ -117,7 +169,7 @@ Sighting.create!(
 )
 
 Sighting.create!(
-  user_id: 4902,
+  user_id: 2,
   date: "2000/10/10",
   time: "早朝",
   area: "大阪城",
@@ -127,7 +179,7 @@ Sighting.create!(
 )
 
 Sighting.create!(
-  user_id: 4902,
+  user_id: 3,
   date: "2000/09/20",
   time: "15:00ごろ",
   area: "名古屋駅",
@@ -138,33 +190,34 @@ Sighting.create!(
 
 # テスト用DMデータ
 Room.create!(
-  user_id: 4902
+  user_id: 1,
+  id: 10
 )
 
 Entry.create!(
-  user_id: 4902,
-  room_id: 781
+  user_id: 1,
+  room_id: 10
 )
 
 Entry.create!(
-  user_id: 4903,
-  room_id: 781
+  user_id: 3,
+  room_id: 10
 )
 
 Message.create!(
-  user_id: 4902,
-  room_id: 781,
+  user_id: 1,
+  room_id: 10,
   message: "見つかりました！"
 )
 
 Message.create!(
-  user_id: 4903,
-  room_id: 781,
+  user_id: 3,
+  room_id: 10,
   message: "良かったです"
 )
 
 Message.create!(
-  user_id: 4902,
-  room_id: 781,
+  user_id: 1,
+  room_id: 10,
   message: "ありがとうございました"
 )
