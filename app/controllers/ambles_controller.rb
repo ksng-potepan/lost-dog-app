97657d81c812f59da097af19135a00c6f512758c
ksng-potepan <min.fam462@gmail.com>
2024-03-01 11:53:40 +0900

    feat: Ambleの詳細画面からDMできるよう変更

--- app/controllers/ambles_controller.rb
+++ app/controllers/ambles_controller.rb
@@ -31,6 +31,22 @@ class AmblesController < ApplicationController
   def show
     @user = current_user
     @amble = Amble.where(id: params[:id])
+    @amble_user = Amble.find_by(id: params[:id])
+    @current_entry = Entry.where(user_id: @user.id)
+    @another_entry = Entry.where(user_id: @amble_user.user.id)
+
+    @current_entry.each do |current|
+      @another_entry.each do |another|
+        if current.room_id == another.room_id
+          @is_room = true
+          @room_id = current.room_id
+        end
+      end
+    end
+    return if @is_room
+
+    @room = Room.new
+    @entry = Entry.new
   end
 
   def new
