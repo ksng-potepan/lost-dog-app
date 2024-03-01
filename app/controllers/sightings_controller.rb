b9850aa9bc1a6565612b0157ac0c2f727e20f81e
ksng-potepan <min.fam462@gmail.com>
2024-03-01 11:54:35 +0900

    feat: Sightingの詳細画面からDMできるよう変更

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -13,6 +13,22 @@ class SightingsController < ApplicationController
   def show
     @user = current_user
     @sighting = Sighting.find(params[:id])
+    @sighting_user = Sighting.find_by(id: params[:id])
+    @current_entry = Entry.where(user_id: @user.id)
+    @another_entry = Entry.where(user_id: @sighting_user.user.id)
+
+    @current_entry.each do |current|
+      @another_entry.each do |another|
+        if current.room_id == another.room_id
+          @is_room = true
+          @room_id = current.room_id
+        end
+      end
+    end
+    return if @is_room
+
+    @room = Room.new
+    @entry = Entry.new
   end
 
   def new
