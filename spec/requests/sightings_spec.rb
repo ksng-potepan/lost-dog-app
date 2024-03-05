ff5b577b8bd8c1e44e08bc19b33abb51fdfca926
ksng-potepan <min.fam462@gmail.com>
2024-03-05 23:19:17 +0900

    feat: RequestSpecへ`edit`と`update`についてテストコードを追加

--- spec/requests/sightings_spec.rb
+++ spec/requests/sightings_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Sightings' do
   let(:user) { create(:user) }
   let(:sighting) { create(:sighting, user:) }
+  let(:params) do
+    {
+      id: sighting.id,
+      date: sighting.date,
+      time: sighting.time,
+      area: sighting.area,
+      lat: sighting.lat,
+      lng: sighting.lng
+    }
+  end
 
   before do
     sign_in user
@@ -57,6 +67,64 @@ RSpec.describe 'Sightings' do
     it '投稿が削除されること' do
       delete sighting_path(sighting)
       expect(Sighting.find_by(id: sighting.id)).to be_nil
+    end
+  end
+
+  describe 'GET /sightings/:id/edit' do
+    before do
+      get edit_sighting_path(sighting.id)
+    end
+
+    it '編集画面の表示に成功すること' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'POST /sightings/update' do
+    context 'パラメータが妥当な場合' do
+      before do
+        params = {
+          sighting: {
+            area: '大阪駅',
+            lat: 34.7024,
+            lng: 135.4959
+          }
+        }
+        patch(sighting_path(sighting), params:)
+        Sighting.find(sighting.id)
+        sighting.reload
+      end
+
+      it 'リクエストが成功すること' do
+        expect(response).to have_http_status(:found)
+      end
+
+      it 'リダイレクトすること' do
+        expect(response).to redirect_to sighting_path(sighting.id)
+      end
+    end
+
+    context 'パラメータが不正な場合' do
+      before do
+        params = {
+          sighting: {
+            area: '',
+            lat: nil,
+            lng: nil
+          }
+        }
+        patch(sighting_path(sighting), params:)
+        Sighting.find(sighting.id)
+        sighting.reload
+      end
+
+      it 'リクエストが成功すること' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'エラーが表示されること' do
+        expect(response.body).to include '更新に失敗しました。'
+      end
     end
   end
 
