ce2be15b23114c7f4a58ae7cf3625e2de3da61f1
ksng-potepan <min.fam462@gmail.com>
2024-03-05 23:43:56 +0900

    feat: systemSpecへ編集についてテストコードを追加

--- spec/system/sightings_spec.rb
+++ spec/system/sightings_spec.rb
@@ -57,6 +57,49 @@ RSpec.describe 'Sightings' do
     end
   end
 
+  describe '投稿編集ページ' do
+    before do
+      visit edit_sighting_path(sighting)
+    end
+
+    context '正常に更新できる場合' do
+      before do
+        fill_in "sighting_date", with: sighting.date
+        fill_in 'sighting_area', with: '大阪駅'
+        find_by_id('lat', visible: false).set(34.7024)
+        find_by_id('lng', visible: false).set(135.4959)
+        click_button '登録する'
+      end
+
+      it '通知メッセージが表示されること' do
+        expect(page).to have_content '更新しました。'
+      end
+
+      it 'amble_pathに遷移すること' do
+        expect(page).to have_current_path sighting_path(Sighting.last)
+      end
+    end
+
+    context '更新に失敗する場合' do
+      it '場所が未記入なこと' do
+        fill_in "sighting_date", with: sighting.date
+        fill_in 'sighting_area', with: ''
+        find_by_id('lat', visible: false).set(nil)
+        find_by_id('lng', visible: false).set(nil)
+        click_button '登録する'
+        expect(page).to have_content 'マーカーを立ててください'
+      end
+
+      it '今日の日付より未来の日付を入力した場合、エラーメッセージが表示されること' do
+        fill_in 'sighting_date', with: (Time.zone.today + 1).to_s
+        fill_in "sighting_area", with: sighting.area
+        find_by_id('lat', visible: false).set(sighting.lat)
+        click_button '登録する'
+        expect(page).to have_content '日付は今日を含む前の日付を登録してください。'
+      end
+    end
+  end
+
   describe '投稿一覧ページ' do
     before do
       visit sightings_path(sighting)
