eac7ab7f5000fa48c8f4133733baf90e1d37c428
ksng-potepan <min.fam462@gmail.com>
2024-03-10 15:46:52 +0900

    fix: 都道府県が選択されていない場合でも投稿できる問題を修正

--- app/models/amble.rb
+++ app/models/amble.rb
@@ -4,8 +4,8 @@ class Amble < ApplicationRecord
 
   validates :name, :size, :gender, :date, :municipalities, presence: true
   validates :features, :situation, length: { maximum: 50 }
+  validates :prefecture, exclusion: { in: ['都道府県'] }
   validate :date_before_start
-  validate :prefecture_cannot_be_zero
 
   def date_before_start
     if date.nil?
@@ -14,9 +14,5 @@ class Amble < ApplicationRecord
       errors.add(:date, "は今日を含む前の日付を登録してください。") unless
       date <= Date.current
     end
-  end
-
-  def prefecture_cannot_be_zero
-    errors.add(:prefecture, "選択してください") if prefecture == 0
   end
 end
